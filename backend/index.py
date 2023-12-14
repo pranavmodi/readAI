b09484fe6ba7d4fdebab43a698d99d81e56fc373
Pranav <pranav.modi@gmail.com>
2023-12-14 10:28:37 +0530

    dir structure changes

--- backend/index.py
+++ backend/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<p> Hello, new fucker World!</p> '
+    return '<p> Hello, new  World!</p> '
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8080)
